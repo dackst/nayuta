e07946ef7b4abf1c3f99c15f6d40adcbcbd2b4ad
D <ds5cn@virginia.edu>
2021-02-11 16:25:46 -0500

    copy_all: allow easy copying to unpacked iso in other directories

--- copy_all.py
+++ copy_all.py
@@ -13,6 +13,9 @@ import shutil
 import packed_lib
 from contextlib import suppress
 
+# choose another unpacked ISO folder if you want
+target_dir = 'ISO'
+
 def packed_update(file_list, packed_list):
     '''
     modify files in packed_list so they don't 
@@ -35,7 +38,7 @@ def packed_update(file_list, packed_list):
 
 def copy_arb():
     src_path = r'arg/output'
-    tgt_path = r'ISO/PSP_GAME/USRDIR/map'
+    tgt_path = target_dir + r'/PSP_GAME/USRDIR/map'
     filelist = os.listdir(src_path)
 
     for filename in filelist:
@@ -44,7 +47,7 @@ def copy_arb():
             '{}/{}/{}.arb'.format(tgt_path, filename, filename)
         )
 
-    # packed_path = r'ISO/PSP_GAME/USRDIR/pack/map'
+    # packed_path = target_dir + r'/PSP_GAME/USRDIR/pack/map'
     # packed_list = tuple(filter(lambda x: '.mpp' in x, os.listdir(packed_path)))
     # packed_list = ['{}/{}'.format(packed_path, i) for i in packed_list]
 
@@ -53,7 +56,7 @@ def copy_arb():
 def copy_script():
     script_path = r'script/output'
     filelist = tuple(filter(lambda x: '.bin' in x, os.listdir(script_path)))
-    text_path = r'ISO/PSP_GAME/USRDIR/script'
+    text_path = target_dir + r'/PSP_GAME/USRDIR/script'
 
     for filename in filelist:
         shutil.copy(
@@ -61,7 +64,7 @@ def copy_script():
             '{}/{}'.format(text_path, filename)
         )
 
-    # packed_path = r'ISO/PSP_GAME/USRDIR/pack/map'
+    # packed_path = target_dir + r'/PSP_GAME/USRDIR/pack/map'
     # packed_list = list(filter(lambda x: '.mpp' in x, os.listdir(packed_path)))
     # packed_list = ['{}/{}'.format(packed_path, i) for i in packed_list]
 
@@ -74,22 +77,23 @@ def copy_img():
     filelist = tuple('{}/{}'.format(src_path, x) for x in filter(
         lambda x: x.startswith('h_') and x.endswith('.itp'), os.listdir(src_path)))
     for filename in filelist:
-        shutil.copy(filename, r'ISO/PSP_GAME/USRDIR/visual/help')
+        shutil.copy(filename, target_dir + r'/PSP_GAME/USRDIR/visual/help')
 
     # chapter start/end titles
     filelist = tuple('{}/{}'.format(src_path, x) for x in filter(
         lambda x:  x.startswith('p_') and x.endswith('.itp'), os.listdir(src_path)))
     for filename in filelist:
-        shutil.copy(filename, r'ISO/PSP_GAME/USRDIR/visual/event')
+        shutil.copy(filename, target_dir + r'/PSP_GAME/USRDIR/visual/event')
 
     #save file images
     for filename in ('{}/{}'.format(src_path, x) for x in (
         'icon_new.png', 'icon0.png', 'pic1.png')):
-        shutil.copy(filename, r'ISO/PSP_GAME/USRDIR/savefile')
-    shutil.copy(r'{}/icon0.png'.format(src_path), r'ISO/PSP_GAME/ICON0.PNG')
-    shutil.copy(r'{}/pic1.png'.format(src_path), r'ISO/PSP_GAME/PIC1.PNG')
+        shutil.copy(filename, target_dir + r'/PSP_GAME/USRDIR/savefile')
+    shutil.copy(r'{}/icon0.png'.format(src_path), target_dir + r'/PSP_GAME/ICON0.PNG')
+    shutil.copy(r'{}/pic1.png'.format(src_path), target_dir + r'/PSP_GAME/PIC1.PNG')
 
-    shutil.copy(r'{}/nyt_op.pmf'.format(src_path), r'ISO/PSP_GAME/USRDIR/movie')
+    # op movie
+    shutil.copy(r'{}/nyt_op.pmf'.format(src_path), target_dir + r'/PSP_GAME/USRDIR/movie')
 
     #"system" images
     filelist = [
@@ -97,18 +101,18 @@ def copy_img():
         'm_mons.itp', 'm_quest.itp', 'title0.it3', 'title1.it3', 'title2.it3']
     for filename in filelist:
         shutil.copy('{}/{}'.format(src_path, filename),
-                    r'ISO/PSP_GAME/USRDIR/system')
+                    target_dir + r'/PSP_GAME/USRDIR/system')
 
     packed_list = [
-        r'ISO/PSP_GAME/USRDIR/pack/global/first.dat',
-        r'ISO/PSP_GAME/USRDIR/pack/global/global.dat'
+        target_dir + r'/PSP_GAME/USRDIR/pack/global/first.dat',
+        target_dir + r'/PSP_GAME/USRDIR/pack/global/global.dat'
     ]
 
     # final boss attack name texture
     shutil.copy('{}/{}'.format(src_path, 'ef_03_21.itp'), 
-                r'ISO/PSP_GAME/USRDIR/efx/tex')
+                target_dir + r'/PSP_GAME/USRDIR/efx/tex')
     filelist.append('ef_03_21.itp')
-    packed_list.append(r'ISO/PSP_GAME/USRDIR/pack/map/mp_8099.mpp')
+    packed_list.append(target_dir + r'/PSP_GAME/USRDIR/pack/map/mp_8099.mpp')
 
     packed_update(filelist, packed_list)
 
@@ -116,7 +120,7 @@ def copy_text():
     src_path = r'text/output'
     filelist = tuple(filter(lambda x: '.tbl' in x or '.tbb' in x,
                         os.listdir(src_path)))
-    tgt_path = r'ISO/PSP_GAME/USRDIR/text'
+    tgt_path = target_dir + r'/PSP_GAME/USRDIR/text'
 
     for filename in filelist:
         shutil.copy(
@@ -124,34 +128,34 @@ def copy_text():
             '{}/{}'.format(tgt_path, filename)
         )
 
-    # packed_path = r'ISO/PSP_GAME/USRDIR/pack/map'
+    # packed_path = target_dir + r'/PSP_GAME/USRDIR/pack/map'
     # packed_list = list(filter(lambda x: '.mpp' in x, os.listdir(packed_path)))
     # packed_list = ['{}/{}'.format(packed_path, x) for x in packed_list]
-    # packed_list.append(r'ISO/PSP_GAME/USRDIR/pack/global/first.dat')
-    # packed_list.append(r'ISO/PSP_GAME/USRDIR/pack/global/global.dat')
+    # packed_list.append(target_dir + r'/PSP_GAME/USRDIR/pack/global/first.dat')
+    # packed_list.append(target_dir + r'/PSP_GAME/USRDIR/pack/global/global.dat')
     # packed_update(filelist, packed_list)
 
     # #questlib.tbb and m_quest.itp are in these
     # #that's all that's there so we're just going to delete them
     # with suppress(FileNotFoundError):
-    #     os.remove(r'ISO/PSP_GAME/USRDIR/pack/script/mp_0004.2pp')
-    #     os.remove(r'ISO/PSP_GAME/USRDIR/pack/script/mp_0004c.2pp')
-    #     os.remove(r'ISO/PSP_GAME/USRDIR/pack/script/mp_0004d.2pp')
+    #     os.remove(target_dir + r'/PSP_GAME/USRDIR/pack/script/mp_0004.2pp')
+    #     os.remove(target_dir + r'/PSP_GAME/USRDIR/pack/script/mp_0004c.2pp')
+    #     os.remove(target_dir + r'/PSP_GAME/USRDIR/pack/script/mp_0004d.2pp')
 
 def copy_eboot_misc():
     #EBOOT
-    shutil.copy(r'EBOOT/EBOOT.BIN', r'ISO/PSP_GAME/SYSDIR')
+    shutil.copy(r'EBOOT/EBOOT.BIN', target_dir + r'/PSP_GAME/SYSDIR')
 
     #PARAM.SFO
-    shutil.copy(r'misc/PARAM.SFO', r'ISO/PSP_GAME')
+    shutil.copy(r'misc/PARAM.SFO', target_dir + r'/PSP_GAME')
 
     #misc files
-    shutil.copy(r'misc/detail.txt', r'ISO/PSP_GAME/USRDIR/savefile')
-    shutil.copy(r'misc/dtitle.txt', r'ISO/PSP_GAME/USRDIR/savefile')
-    shutil.copy(r'misc/title.txt', r'ISO/PSP_GAME/USRDIR/savefile')
+    shutil.copy(r'misc/detail.txt', target_dir + r'/PSP_GAME/USRDIR/savefile')
+    shutil.copy(r'misc/dtitle.txt', target_dir + r'/PSP_GAME/USRDIR/savefile')
+    shutil.copy(r'misc/title.txt', target_dir + r'/PSP_GAME/USRDIR/savefile')
 
     #delete UPDATE folder (not needed)
-    shutil.rmtree(r'ISO/PSP_GAME/SYSDIR/UPDATE', ignore_errors = True)
+    shutil.rmtree(target_dir + r'/PSP_GAME/SYSDIR/UPDATE', ignore_errors = True)
 
 
 if __name__ == "__main__":
